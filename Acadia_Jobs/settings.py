2feb696895a1c055dc15ac850a4ba0c7371b9e71
roshan <roshanadhikari0420@gmail.com>
2025-07-30 23:12:19 -0300

    Settings.py

--- Acadia_Jobs/settings.py
+++ Acadia_Jobs/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 ]
 
 TAILWIND_APP_NAME = 'theme'
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = "/usr/local/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
